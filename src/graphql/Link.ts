fffa398647b8b3e3f6f69e946270ec13a9e7415f
Zuan0x <zuan.0x@gmail.com>
2023-10-25 08:28:44 +0100

    Add create link functionality

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -1,5 +1,6 @@
-import { extendType, objectType } from "nexus";
+import { extendType, nonNull, objectType, stringArg } from "nexus";   
 import { NexusGenObjects } from "../../nexus-typegen";  
+
 
 export const Link = objectType({
     name: "Link", // <- Name of your type
@@ -30,6 +31,32 @@ export const LinkQuery = extendType({
             type: "Link",
             resolve(parent, args, context, info) {
                 return links;
+            },
+        });
+    },
+});
+
+export const LinkMutation = extendType({ 
+    type: "Mutation",    
+    definition(t) {
+        t.nonNull.field("post", { 
+            type: "Link",  
+            args: {   // 3
+                description: nonNull(stringArg()),
+                url: nonNull(stringArg()),
+            },
+            
+            resolve(parent, args, context) {    
+                const { description, url } = args; 
+                
+                let idCount = links.length + 1; 
+                const link = {
+                    id: idCount,
+                    description: description,
+                    url: url,
+                };
+                links.push(link);
+                return link;
             },
         });
     },
